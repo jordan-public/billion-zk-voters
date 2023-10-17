cd2f24724c7078b172e85012aa4c03257d635c16
jordan <jordan@bitola.com>
2023-10-17 05:43:42 -0400

    Vote display contract reading

--- display/displayui/src/App.js
+++ display/displayui/src/App.js
@@ -10,28 +10,19 @@ import {
   theme,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import Body from './components/Body';
+import { ethers } from 'ethers';
+
+const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
+      <Box fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
+            <Body provider={provider}/>
           </VStack>
         </Grid>
       </Box>
---
+++ display/displayui/src/components/Body.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import {
+    ChakraProvider,
+    Box,
+    Text,
+    Link,
+    VStack,
+    Code,
+    Grid,
+    InputGroup,
+    Input,
+    InputLeftAddon,
+    List,
+    ListItem,
+    theme,
+  } from '@chakra-ui/react';
+import { ethers } from 'ethers';
+import aAggregateCounts from '../artifacts/AggregateCounts.json';
+import aMockVerifier from '../artifacts/MockVerifier.json';
+
+const Body = ({provider}) => {
+    const [blockNumber, setBlockNumber] = React.useState(0);
+    const [cAggregateCounts, setCAggregateCounts] = React.useState(null);
+    const [cMockVerifier, setCMockVerifier] = React.useState(null);
+    const [issue, setIssue] = React.useState('');
+    const [issueHash, setIssueHash] = React.useState(null);
+    const [title, setTitle] = React.useState('');
+    const [options, setOptions] = React.useState([]);
+    const [votes, setVotes] = React.useState([]);
+
+    React.useEffect(() => {
+        if (!provider) return;
+        setCAggregateCounts(new ethers.Contract(aAggregateCounts.contractAddress, aAggregateCounts.abi, provider));
+        setCMockVerifier(new ethers.Contract(aMockVerifier.contractAddress, aMockVerifier.abi, provider));
+    }, [provider]);
+
+    React.useEffect(() => {
+        (async () => {
+console.log("cAggregateCounts: ", cAggregateCounts);
+            if (!cAggregateCounts) return;
+            if (options.length === 0) return;
+            if (!issueHash) return;
+            try {
+                let v = [];
+                for (let i = 0; i < options.length; i++) {
+                    v.push(await cAggregateCounts.getVoteCount(issueHash, i));
+                };
+                setVotes(v);
+            } catch (error) {
+                console.log("error: ", error);
+            }
+            })();
+    }, [blockNumber]);
+
+    React.useEffect(() => {
+        if (provider) {
+            provider.on("block", setBlockNumber);
+            return () => provider.off("block", setBlockNumber);
+        }
+    }, [provider]);
+
+    React.useEffect(() => {
+        if (!issue) return;
+        const h = ethers.keccak256(ethers.toUtf8Bytes(issue));
+        console.log("issueHash: ", h);
+        const parts = issue.split('|');
+        setTitle(parts[0]);
+        setOptions(parts.slice(1));
+    }, [issue]);
+
+    return (<VStack spacing={8}>
+        <Box>
+            <InputGroup>
+                <InputLeftAddon color="black" children='Issue' />
+                <Input placeholder='type issue text here' value={issue} onChange={(e) => setIssue(e.target.value)}/>
+            </InputGroup>
+        </Box>
+        <Box justifySelf>
+            <Text fontSize="6xl" color="tomato">Issue: {title}</Text>
+            <List>
+                {options.map((o, i) => <ListItem key={i}><Text fontSize="6xl" color="tomato">{o}: {votes && votes[i] && votes[i]}</Text></ListItem>)}
+            </List>
+            <Text fontSize="6xl" color="tomato">Block number: {blockNumber}</Text>
+        </Box>
+    </VStack>);
+}
+
+  export default Body;
