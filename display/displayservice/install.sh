5ddaef3e52993f188519ecbd4ee5a2a4dd332dd5
jordan <jordan@bitola.com>
2023-10-17 19:11:56 -0400

    plonk_vk.sol creation and script fixes

--- display/displayservice/install.sh
+++ display/displayservice/install.sh
@@ -2,10 +2,6 @@
 
 #!/bin/bash
 
-pushd ../sumvproof/
-nargo check
-nargo compile
-popd
 mkdir -p circuits
 cp ../sumvproof/target/* circuits/
 ln -sf ../displayui/src/artifacts artifacts
---
+++ display/sumvproof/install.sh
@@ -0,0 +1,4 @@
+nargo check
+nargo compile
+nargo codegen-verifier
+cp contract/sumvproof/plonk_vk.sol ../src/
