b1549b6008d3f66bff2a8576a6411cf5f76438ee
jordan <jordan@bitola.com>
2023-10-17 18:29:52 -0400

    displayservice and sumvproof skeleton

--- display/displayservice/install.sh
+++ display/displayservice/install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-ln -s ../displayui/src/artifacts artifacts
+#!/bin/bash
+
+pushd ../sumvproof/
+nargo check
+nargo compile
+popd
+mkdir -p circuits
+cp ../sumvproof/target/* circuits/
+ln -sf ../displayui/src/artifacts artifacts
